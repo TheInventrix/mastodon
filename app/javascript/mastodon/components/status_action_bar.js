6a37cf9bc85825d3261653d19500e2188dd39f6c
TheInventrix <TheInventrix@users.noreply.github.com>
2018-08-29 18:26:52 -0600

    change local-only "reblog" icon to a globe
    also make it disabled in expanded view as well

--- app/javascript/mastodon/components/status_action_bar.js
+++ app/javascript/mastodon/components/status_action_bar.js
@@ -186,8 +186,10 @@ export default class StatusActionBar extends ImmutablePureComponent {
 
     if (status.get('visibility') === 'direct') {
       reblogIcon = 'envelope';
-    } else if ((status.get('visibility') === 'private') || (status.get('visibility') === 'local')) {
+    } else if (status.get('visibility') === 'private') {
       reblogIcon = 'lock';
+    } else if (status.get('visibility') === 'local') {
+      reblogIcon = 'globe';
     }
 
     if (status.get('in_reply_to_id', null) === null) {
